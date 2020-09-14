836bad41cd4d7b47439bfe8f476401991b870368
Chantal Snazel <clsnazel@gmail.com>
2020-09-14 22:02:57 +0000

    Created and completed head.js

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  if (arr.length > 0) {
+    return arr[0];
+  } else {
+    return undefined;
+  }
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5,6,7]), 7);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
