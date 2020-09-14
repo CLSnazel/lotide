6a0cb72e75cf2e1610ba4cddc9fe4762c166c01f
Chantal Snazel <clsnazel@gmail.com>
2020-09-14 21:46:35 +0000

    Created assertEqual w/ Emojis

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\u26D4" + " Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("\u2705" + " Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", 1);
+assertEqual(1 ,1);
+assertEqual(1 ,8);
