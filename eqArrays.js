369429330144687d23ea772ebb7c300c80f03d15
Chantal Snazel <clsnazel@gmail.com>
2020-09-15 02:12:08 +0000

    Created and completed eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actualArr, expectedArr) {
+  //arrays not the same size, return false
+  if (actualArr.length !== expectedArr.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actualArr.length; i++) {
+    //items at index i do not match, return false
+    if (actualArr[i] !== expectedArr[i]) {
+      return false;
+    }
+  }
+
+  //if we get here, it passes
+  return true;
+};
+
+assertEqual(eqArrays([1,2,3], [1,2,3]), true);
+assertEqual(eqArrays([1,2,3], [1,2]), false);
+assertEqual(eqArrays([1,2,3], [1,2,4]), false);
