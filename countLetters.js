64984cf6e06a420d4d875859bbd0b1f09eebf47c
Chantal Snazel <clsnazel@gmail.com>
2020-09-16 01:37:28 +0000

    created and completed countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(inputString) {
+  if (inputString.length < 1) {
+    return {};
+  }
+  let letterCount = {};
+  for (let char of inputString) {
+    if (char !== " ") {
+      if (letterCount[char]) {
+        letterCount[char] += 1;
+      } else {
+        letterCount[char] = 1;
+      }
+    }
+  }
+  return letterCount;
+};
+
+const result1 = countLetters("lighthouse in the house");
+assertEqual(result1["l"], 1);
+assertEqual(result1['i'], 2);
+assertEqual(result1["g"], 1);
+assertEqual(result1["t"], 2);
+assertEqual(result1["o"], 2);
+assertEqual(result1["u"], 2);
+assertEqual(result1['s'], 2);
+assertEqual(result1["e"], 3);
+assertEqual(result1["n"], 1);
