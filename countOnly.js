06c7439c4ec5e45806458b61ef8f343cb034d2d9
Chantal Snazel <clsnazel@gmail.com>
2020-09-16 01:17:08 +0000

    Created and completed countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(arr, countObj) {
+  if (arr.length === 0 || countObj === {}) {
+    return {};
+  }
+  const results = {};
+  for (const item of arr) {
+    //console.log(item)
+    
+    //check key in results
+    if (countObj[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+
+const firstNames = [
+  'Karl',
+  'Salima',
+  'Agouhanna',
+  'Fang',
+  'Kavith',
+  'Jason',
+  'Salima',
+  'Fang',
+  'Joe'
+];
+
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang":true, "Agouhanna":false});
+
+assertEqual(result1['Jason'], 1);
+assertEqual(result1['Karima'], undefined);
+assertEqual(result1['Fang'], 2);
+assertEqual(result1["Agouhanna"], undefined);
