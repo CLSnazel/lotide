1a2c89f339c966799abe7fef2301486de3e127b3
Chantal Snazel <clsnazel@gmail.com>
2020-09-16 02:17:04 +0000

    Created and completed findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  let allKeys = Object.keys(obj);
+  for (let key of allKeys) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const testObj1 = {"hello": 2, "meow": "cats"};
+
+assertEqual(findKeyByValue(testObj1, "cats"), "meow");
+assertEqual(findKeyByValue(testObj1, 2), "hello");
+assertEqual(findKeyByValue(testObj1, "jubilee"), undefined);
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
